8e1f893f05ae53a4b0c94482b19c44348f534e33
Paolo Invernizzi <invernp91@gmail.com>
2020-08-21 21:17:55 +0200

    Adding Spring Config. Closes #22

--- pet-clinic-data/src/main/java/it/ttf/invernizzi/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/it/ttf/invernizzi/petclinic/services/map/VetServiceMap.java
@@ -2,9 +2,10 @@ package it.ttf.invernizzi.petclinic.services.map;
 
 import it.ttf.invernizzi.petclinic.model.Vet;
 import it.ttf.invernizzi.petclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
-
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findall() {
