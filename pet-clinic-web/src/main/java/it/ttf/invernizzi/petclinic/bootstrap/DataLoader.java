53a3f0861ac607f9da145944404d4457ecacec7d
Paolo Invernizzi <invernp91@gmail.com>
2020-08-21 21:30:40 +0200

    Adding Owners' list and updated controller. Closes #16

--- pet-clinic-web/src/main/java/it/ttf/invernizzi/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/it/ttf/invernizzi/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import it.ttf.invernizzi.petclinic.model.Owner;
 import it.ttf.invernizzi.petclinic.model.Vet;
 import it.ttf.invernizzi.petclinic.services.OwnerService;
 import it.ttf.invernizzi.petclinic.services.VetService;
-import it.ttf.invernizzi.petclinic.services.map.OwnerServiceMap;
-import it.ttf.invernizzi.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
--- pet-clinic-web/src/main/java/it/ttf/invernizzi/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/it/ttf/invernizzi/petclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package it.ttf.invernizzi.petclinic.controllers;
 
+import it.ttf.invernizzi.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({ "", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findall());
         return "owners/index";
     }
 }
