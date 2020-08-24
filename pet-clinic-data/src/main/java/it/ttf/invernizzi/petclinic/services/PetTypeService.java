b305d40545c6a8227ecb01f01faa694185177550
Paolo Invernizzi <invernp91@gmail.com>
2020-08-24 17:02:30 +0200

    Added Pet Type Map Service. Closes #28

---
+++ pet-clinic-data/src/main/java/it/ttf/invernizzi/petclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package it.ttf.invernizzi.petclinic.services;
+
+import it.ttf.invernizzi.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+
+}
---
+++ pet-clinic-data/src/main/java/it/ttf/invernizzi/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,33 @@
+package it.ttf.invernizzi.petclinic.services.map;
+
+import it.ttf.invernizzi.petclinic.model.PetType;
+import it.ttf.invernizzi.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findall() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteByID(aLong);
+    }
+
+    @Override
+    public void delete(PetType obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public PetType save(PetType obj) {
+        return super.save(obj);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
