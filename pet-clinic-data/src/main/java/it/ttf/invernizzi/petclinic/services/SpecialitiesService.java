c0f7e4936a61f6ab007478b54f3deb0640f8f824
Paolo Invernizzi <invernp91@gmail.com>
2020-08-24 18:11:21 +0200

    Added Specialities map Service . Closes #30

---
+++ pet-clinic-data/src/main/java/it/ttf/invernizzi/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package it.ttf.invernizzi.petclinic.services;
+
+import it.ttf.invernizzi.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/it/ttf/invernizzi/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package it.ttf.invernizzi.petclinic.services.map;
+
+import it.ttf.invernizzi.petclinic.model.Speciality;
+import it.ttf.invernizzi.petclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findall() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public Speciality save(Speciality obj) {
+        return super.save(obj);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteByID(aLong);
+    }
+}
