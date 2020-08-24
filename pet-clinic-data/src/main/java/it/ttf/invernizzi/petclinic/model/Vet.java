b88d2eb0a6261d2d0b1d500438d4abd2b8f7ad63
Paolo Invernizzi <invernp91@gmail.com>
2020-08-24 16:14:43 +0200

    Added specialities, added to vet. Closes #25

--- pet-clinic-data/src/main/java/it/ttf/invernizzi/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/it/ttf/invernizzi/petclinic/model/Vet.java
@@ -1,7 +1,16 @@
 package it.ttf.invernizzi.petclinic.model;
 
+import java.util.Set;
+
 public class Vet  extends Person{
 
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
 
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
